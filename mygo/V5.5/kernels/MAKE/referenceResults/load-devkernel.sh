af9e236848a4b27d62398bcd9c374fdc26570e13
prof-braino <prof.braino@gmail.com>
2016-01-09 15:48:10 -0600

    these were untracked, so I added them individually

---
+++ mygo/V5.5/kernels/MAKE/referenceResults/load-devkernel.sh
@@ -0,0 +1,10 @@
+#bash 
+# this script is for linux 64 (Linux Mint 17.2 MATE)
+
+# bash -v ./propeller-load -p /dev/ttyUSB0 -e PF5.5DevKernel.eeprom
+# ./propeller-load -p /dev/ttyUSB0 -e PF5.5DevKernel.eeprom
+# ./propeller-load -p /dev/ttyUSB0 -e /home/braino/PropForth5.5/mygo/V5.5/kernels/DevKernel.eeprom
+# ./propeller-load -p /dev/ttyUSB0 -e  /home/braino/PropForth5.5/mygo/V5.5/kernels/MAKE/results/outputFiles/DevKernel.eeprom
+
+
+./propeller-load -p /dev/ttyUSB0 -e -r /home/braino/PropForth5.5/mygo/V5.5/kernels/MAKE/referenceResults/outputFiles/DevKernel.eeprom
